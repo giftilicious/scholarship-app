af7af5b7a197fe3ca59df677b1ea9196552e25d2
Altug Cakmakci <altugcakmakci@gmail.com>
2021-11-25 11:04:34 -0500

    provider new scholarship component

---
+++ client/src/components/ScholarshipForm/data.js
@@ -0,0 +1,27 @@
+export const ethnicityOptions = [
+    { value: "African American", label: "African American" },
+    { value: "Indigenous", label: "Indigenous" },
+    { value: "Asian", label: "Asian"},
+    { value: "Hispanic", label: "Hispanic" },
+    { value: "Caucasian", label: "Caucasian" }
+  ];
+
+export const genderOptions = [
+    { value: "Male", label: "Male" },
+    { value: "Female", label: "Female" },
+    { value: "Gender diverse", label: "Gender diverse"}
+  ];
+
+export const disabilityOptions = [
+    { value: "Physical", label: "Physical" },
+    { value: "Intellectual", label: "Intellectual" },
+    { value: "Visual", label: "Visual" },
+    { value: "Hearing", label: "Hearing" },
+    { value: "Psychiatric", label: "Psychiatric"}
+  ];
+
+export const levelofstudyOptions = [
+    { value: "Postgrad", label: "Postgrad" },
+    { value: "Undergrad", label: "Undergrad" },
+    { value: "High school", label: "High school" }
+  ];
