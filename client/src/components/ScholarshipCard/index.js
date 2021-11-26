c68c414a48b209e8a40f0f27e24ef4fb54422afc
Andres Pena <penchjpc@gmail.com>
2021-11-26 14:26:29 -0500

    Formatting of ScholarshipCard

--- client/src/components/ScholarshipCard/index.js
+++ client/src/components/ScholarshipCard/index.js
@@ -4,7 +4,7 @@ const ScholarshipCard = ({ scholarship }) => {
 
   return (
     <div>
-        <div key={scholarship._id} className="card mb-3">
+        {/* <div key={scholarship._id} className="card mb-3">
             <h4 className="card-header bg-primary text-light p-2 m-0">
               {scholarship.type} <br />
             </h4>
@@ -20,7 +20,24 @@ const ScholarshipCard = ({ scholarship }) => {
               <p>Application link: {scholarship.applink}</p>
               <p>Application e-mail: {scholarship.appemail}</p>
             </div>
-          </div>
+          </div> */}
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{scholarship.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${scholarship.amount}</h6>
+            <h7 class="card-subtitle mb-2 text-muted">Eligibility</h7>                   
+            <p class="card-text">Gender: {scholarship.gender}</p>
+            <p class="card-text">Ethinicity: {scholarship.ethnicity}</p>          
+            <p class="card-text">Disability: {scholarship.disability}</p>
+            <p class="card-text">Level of Study: {scholarship.levelofstudy}</p>          
+            <p class="card-text">Application deadline: {scholarship.deadline}</p>
+            <p class="card-text">{scholarship.description}</p>       
+            <a href={scholarship.applink} class="card-link">Apply</a>
+            <a href={"mailto:"+ scholarship.appemail} class="card-link">Email us</a>
+        </div>
+    </div>
+
+
     </div>
   );
 };
