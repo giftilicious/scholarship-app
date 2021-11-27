cbc48b6a5a1a8adf2e562ffcde5637dbcc230b6f
Altug Cakmakci <altugcakmakci@gmail.com>
2021-11-27 13:16:37 -0500

    ProvideScholarship.js

--- client/src/pages/ProvideScholarship.js
+++ client/src/pages/ProvideScholarship.js
@@ -1,49 +1,19 @@
-import { useQuery, useMutation } from '@apollo/client';
-// import { useParams, Link } from 'react-router-dom';
-import { ADD_SCHOLARSHIP } from '../utils/mutations';
-import { QUERY_ME } from '../utils/queries';
+import React from 'react';
 import ScholarshipForm from '../components/ScholarshipForm';
-
-const Award = () => {
-  const {loading, data} = useQuery(QUERY_ME)
-  const award = data?.me || {}
-  // const [addScholarship, { error }] = useMutation(ADD_SCHOLARSHIP)
-
-  // This function will handle the click event to delete the scholarship from the collection
-  // const handleAddScholarship = async (scholarshipId) => {
-    
-  //   if (!token) {
-  //     return false;
-  //   }
-
-  //   try {
-  //     const {data} = await addScholarship({
-  //       variables: {scholarshipId}
-  //     })
-
-  //     addScholarship(scholarshipId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+const ProvideScholarship = () => {
 
   return (
-         <div>
-        {Auth.loggedIn ? (
-           <ScholarshipForm />
-          ) : (
-            <div>
-          {/* Define elements that will render on the screen if user is not logged in */}
-          <h4>
-        You need to be logged in to add awards. Use the navigation
-        links above to sign up or log in!
-      </h4>
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          <ScholarshipForm />
         </div>
-          )}
-        </div>
-      
+      </div>
+    </main>
   );
 };
 
-export default Award;
-
+export default ProvideScholarship;
