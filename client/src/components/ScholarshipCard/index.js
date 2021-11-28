e24dd5a4a25b1cf021ec1bc76e5d7ded65644e77
Summer <svilleneuve1994@gmail.com>
2021-11-28 13:53:39 -0500

    updating react scholarship card component

--- client/src/components/ScholarshipCard/index.js
+++ client/src/components/ScholarshipCard/index.js
@@ -3,43 +3,76 @@ import React from 'react';
 const ScholarshipCard = ({ scholarship }) => {
 
   return (
-    <div>
-        {/* <div key={scholarship._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {scholarship.type} <br />
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{scholarship.title}</p>
-              <p>Value: ${scholarship.amount}</p>
-              <h5>Eligibility</h5>
-              <p>Ethinicity: {scholarship.ethnicity}</p>
-              <p>Disability: {scholarship.disability}</p>
-              <p>Level of Study: {scholarship.levelofstudy}</p>
-              <p>Gender: {scholarship.gender}</p>
-              <p>Application deadline: {scholarship.deadline}</p>
-              <p>Application link: {scholarship.applink}</p>
-              <p>Application e-mail: {scholarship.appemail}</p>
-            </div>
-          </div> */}
-    <div className="card" style= {{ width:'18rem' }}>
-        <div className="card-body">
-            <h5 className="card-title">{scholarship.title}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">${scholarship.amount}</h6>
-            <h6 className="card-subtitle mb-2 text-muted">Eligibility</h6>                   
-            <p className="card-text">Gender: {scholarship.gender}</p>
-            <p className="card-text">Ethinicity: {scholarship.ethnicity}</p>          
-            <p className="card-text">Disability: {scholarship.disability}</p>
-            <p className="card-text">Level of Study: {scholarship.levelofstudy}</p>          
-            <p className="card-text">Application deadline: {scholarship.deadline}</p>
-            <p className="card-text">{scholarship.description}</p>       
-            <a href={scholarship.applink} className="card-link">Apply</a>
-            <a href={"mailto:"+ scholarship.appemail} className="card-link">Email us</a>
+    <div className="col">
+    <div className="card shadow-sm">
+      <div className="card-img-top">
+          <div className="d-flex justify-content-end p-4">
+            <i className="far fa-star fa-lg	favourite-icon"></i>
+          </div>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{scholarship.title}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">${scholarship.amount}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">Eligibility</h6>                   
+        <p className="card-text">Gender: {scholarship.gender}</p>
+        <p className="card-text">Ethinicity: {scholarship.ethnicity}</p>          
+        <p className="card-text">Disability: {scholarship.disability}</p>
+        <p className="card-text">Level of Study: {scholarship.levelofstudy}</p>          
+        <p className="card-text">Application deadline: {scholarship.deadline}</p>
+        <p className="card-text">{scholarship.description}</p>       
+        <a href={scholarship.applink} className="card-link">Apply</a>
+        <a href={"mailto:"+ scholarship.appemail} className="card-link">Email us</a>
+      </div>
+      <div className="card-footer">
+        <div className="d-flex align-items-center">
+          <div className="btn-group w-100">
+            <button type="button" className="btn btn-sm btn-outline-fill">View</button>
+            <button type="button" className="btn btn-sm btn-outline-fill">Edit</button>
+            <button type="button" className="btn btn-sm btn-outline-fill">Apply</button>
+          </div>
         </div>
+      </div>
     </div>
-
-
-    </div>
+  </div>
   );
 };
 
 export default ScholarshipCard;
+
+
+    // <div>
+    //     <div key={scholarship._id} className="card mb-3">
+    //         <h4 className="card-header bg-primary text-light p-2 m-0">
+    //           {scholarship.type} <br />
+    //         </h4>
+    //         <div className="card-body bg-light p-2">
+    //           <p>{scholarship.title}</p>
+    //           <p>Value: ${scholarship.amount}</p>
+    //           <h5>Eligibility</h5>
+    //           <p>Ethinicity: {scholarship.ethnicity}</p>
+    //           <p>Disability: {scholarship.disability}</p>
+    //           <p>Level of Study: {scholarship.levelofstudy}</p>
+    //           <p>Gender: {scholarship.gender}</p>
+    //           <p>Application deadline: {scholarship.deadline}</p>
+    //           <p>Application link: {scholarship.applink}</p>
+    //           <p>Application e-mail: {scholarship.appemail}</p>
+    //         </div>
+    //       </div>
+    // <div className="card" style= {{ width:'18rem' }}>
+    //     <div className="card-body">
+    //         <h5 className="card-title">{scholarship.title}</h5>
+    //         <h6 className="card-subtitle mb-2 text-muted">${scholarship.amount}</h6>
+    //         <h6 className="card-subtitle mb-2 text-muted">Eligibility</h6>                   
+    //         <p className="card-text">Gender: {scholarship.gender}</p>
+    //         <p className="card-text">Ethinicity: {scholarship.ethnicity}</p>          
+    //         <p className="card-text">Disability: {scholarship.disability}</p>
+    //         <p className="card-text">Level of Study: {scholarship.levelofstudy}</p>          
+    //         <p className="card-text">Application deadline: {scholarship.deadline}</p>
+    //         <p className="card-text">{scholarship.description}</p>       
+    //         <a href={scholarship.applink} className="card-link">Apply</a>
+    //         <a href={"mailto:"+ scholarship.appemail} className="card-link">Email us</a>
+    //     </div>
+    // </div>
+
+
+    // </div>
